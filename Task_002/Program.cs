d43925af2dfb4716356da7818581e920b1e2da3c
Yulia Gordienko <julia.yanovna@gmail.com>
2023-12-14 22:38:09 +0300

    Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,56 @@
+﻿﻿// Задача 2: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int row, int column, int leftRange, int rightRange)
+{   
+    int[,] matrix = new int[row, column];
+    Random rand = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");  
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapingRow(int[,] matrix)
+{
+    int temp;
+    for(int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0,i];
+        matrix[0,i] = matrix[matrix.GetLength(0)-1, i];
+        matrix[matrix.GetLength(0)-1, i] = temp;
+    }
+}
+
+int row = ReadInt("Введите количество строк 2-х мерного массива: ");
+int column = ReadInt("Введите количество столбцов 2-х мерного массива: ");
+int[,] myMatrix = GenerateMatrix(row, column, -9, 10);
+PrintMatrix(myMatrix);
+SwapingRow(myMatrix);
+Console.WriteLine("__________________________________");
+PrintMatrix(myMatrix);
