e2cc71a4b91e5a12fac80f2f74f95559d2371513
Yulia Gordienko <julia.yanovna@gmail.com>
2023-12-14 22:42:26 +0300

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая 
+// будет находить строку с наименьшей суммой элементов.
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int row, int column, int leftRange, int rightRange)
+{   
+    int[,] matrix = new int[row, column];
+    Random rand = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");  
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindMinSumRow(int[,] matrix)
+{
+    int minRowIndex = 0;
+    int minsum = int.MaxValue;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (sum < minsum){
+            minsum = sum;
+            minRowIndex = i;
+        }
+    }
+    return minRowIndex;
+}
+
+int row = ReadInt("Введите количество строк 2-х мерного массива: ");
+int column = ReadInt("Введите количество столбцов 2-х мерного массива: ");
+int[,] myMatrix = GenerateMatrix(row, column, -9, 10);
+PrintMatrix(myMatrix);
+
+int minRowIndex = FindMinSumRow(myMatrix);
+Console.WriteLine($"Идекс строки массива с наиеньшей суммой элеметов = {minRowIndex}");
